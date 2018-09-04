fa50078e76b283ec7a0eaa5d95b05ae241543b9a
Hidayat arghandabi <hidayatarg@gmail.com>
2018-09-04 14:05:43 +0300

    Routing Architecture added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
+
+import { Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+// Router for angular
+import{RouterModule} from '@angular/router'
+// Router.ts
+import { appRoutes } from "./routes";
 
 import { AppComponent } from './app.component';
 import { ValueComponent } from './value/value.component';
@@ -9,7 +15,9 @@ import { CityComponent } from './city/city.component';
 
 @NgModule({
   declarations: [AppComponent, ValueComponent, NavComponent, CityComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, 
+  RouterModule.forRoot(appRoutes),
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
