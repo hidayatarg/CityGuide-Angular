6091bdfea8368ddd88b9b2c32ae775f7ad28f36a
Hidayat arghandabi <hidayatarg@gmail.com>
2018-09-05 15:48:17 +0300

    Log Out, LoggedIn method implementation

---
+++ src/app/models/registerUser.ts
@@ -0,0 +1,6 @@
+
+
+export class RegisterUser {
+    userName: string;
+    password: string;
+}
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -3,8 +3,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { LoginUser } from '../models/loginUser';
 import { HttpHeaders } from '@angular/common/http';
-import { JwtHelper } from 'angular2-jwt';
+import { JwtHelper, tokenNotExpired } from 'angular2-jwt';
 import { Router } from '@angular/router';
+import { RegisterUser } from '../models/registerUser';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +13,8 @@ import { Router } from '@angular/router';
 export class AuthService {
 
   constructor(private httpClient: HttpClient,
-              private alertifyService:AlertifyService, 
-              private router:Router
+    private alertifyService: AlertifyService,
+    private router: Router
   ) { }
 
   path = "http://localhost:51788/api/auth";
@@ -24,6 +25,8 @@ export class AuthService {
 
   // For angular2-Jwt
   jwtHelper: JwtHelper = new JwtHelper();
+
+  TOKEN_KEY="toke"
 
   // Take a loginUser of Type *(mode)LoginUser
   login(loginUser: LoginUser) {
@@ -39,15 +42,39 @@ export class AuthService {
         // Token during app
         this.userToken = data['tokenString'];
         this.decodedToken = this.jwtHelper.decodeToken(data["tokenString"]);
-       //Alertify Message
-        this.alertifyService.success("Sign in Successfully")
+        //Alertify Message
+        this.alertifyService.success("Sign in Successfully");
         this.router.navigateByUrl('/city');
+
+      });
+  }
+
+  register(registerUser: RegisterUser) {
+    let headers = new HttpHeaders();
+    headers = headers.append("Content-Type", "application/json");
+    this.httpClient.post(this.path + 'register', registerUser, { headers:headers }).
+      subscribe(data => {
 
       });
   }
 
   // Save Token to local storage
   saveToken(token) {
-    localStorage.setItem('token', token);
+    localStorage.setItem(this.TOKEN_KEY, token);
+  }
+
+  logOut(){
+    localStorage.removeItem(this.TOKEN_KEY)
+  }
+
+  // Status: Currently Logged In?
+  loggedIn(){
+    // Token status 
+    return tokenNotExpired(this.TOKEN_KEY);
+  }
+
+  getCurrentUserId(){
+    // Jwt standard
+    return this.jwtHelper.decodeToken(localStorage.getItem(this.TOKEN_KEY)).nameid
   }
 }
